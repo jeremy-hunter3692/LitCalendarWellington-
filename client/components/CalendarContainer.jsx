b86086fd1b8672ce58de21bb8349f5c0d45fdbee
Jeremy Hunter <jeremy.w.hunter@gmail.com>
2022-11-15 18:20:22 +1300

    basic pop up working

--- client/components/CalendarContainer.jsx
+++ client/components/CalendarContainer.jsx
@@ -16,6 +16,7 @@ end.setHours(hour)
 export default function MyCalendar() {
   const [displayPop, setDisplayPop] = useState(false)
   const [popDetails, setPopDetails] = useState({})
+  const [mousePos, setMousePos] = useState({})
 
   const dispatch = useDispatch()
   const eventData = useSelector((state) => state.events)
@@ -33,9 +34,33 @@ export default function MyCalendar() {
         twitter: 'www.t',
       },
     },
-    // { title: 'book event', start: new Date(), end: new Date() },
+    {
+      title: 'Kates Kalandar',
+      start: now,
+      end: end,
+      type: 'author talk',
+      location: 'unity books',
+      imageURL: 'www.whatever',
+      sociallinks: {
+        facebook: 'www.f',
+        instagram: 'www.i',
+        twitter: 'www.t',
+      },
+    },
   ])
-  console.log(events)
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setMousePos({ x: event.clientX, y: event.clientY })
+    }
+
+    window.addEventListener('mousemove', handleMouseMove)
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove)
+    }
+  }, [displayPop])
+  console.log(mousePos)
   // const [newEvents, setNewEvents] = useState([])
 
   // useEffect(() => {
@@ -43,10 +68,32 @@ export default function MyCalendar() {
   //   eventData[0] ? setEvents(eventData) : setEvents([])
   // }, [eventData])
 
+  //////from docs
+  // function useCallback (){(event, start, end, isSelected) => ({
+  //   ...(isSelected && {
+  //     style: {
+  //       backgroundColor: '#000',
+  //     },
+  //   }),
+  //   ...(moment(start).hour() < 12 && {
+  //     className: 'powderBlue',
+  //   }),
+  //   ...(event.title.includes('Meeting') && {
+  //     className: 'darkGreen',
+  //   }),
+  // }),
+  // []}
+
+  // function EventPropGetter() {
+  //   const eventPropGetter = useCallback(event, start, end, isSelected)
+
+  // }
+  //////from docs
+
   function handleSelect(e) {
     //Add newEvent to redux state
     console.log('handle select', e)
-    setDisplayPop(true)
+    setDisplayPop(!displayPop)
     setPopDetails(e)
   }
 
@@ -59,22 +106,21 @@ export default function MyCalendar() {
     <div>
       <button onClick={submit}>submit events</button>
       <h1>Calender</h1>
-      {displayPop ? (
-        <PopupEvent details={popDetails} />
-      ) : (
-        <Calendar
-          localizer={localizer}
-          events={events}
-          startAccessor="start"
-          endAccessor="end"
-          style={{ height: 700 }}
-          defaultView={Views.MONTH}
-          selectable={true}
-          onSelectEvent={handleSelect}
-          // onSelectSlot={handleSelect}
-          longPressThreshold={10}
-        />
-      )}
+
+      {displayPop && <PopupEvent details={popDetails} styleData={mousePos} />}
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 700 }}
+        defaultView={Views.MONTH}
+        // selectable={true}
+        onSelectEvent={handleSelect}
+        // eventPropGetter={(event, start, end, isSelected) => {className: string, style?: Object}}
+        longPressThreshold={10}
+      />
     </div>
   )
 }
--- client/components/PopupEvent.jsx
+++ client/components/PopupEvent.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Popup({ details }) {
+export default function Popup({ details, styleData }) {
+  const absolute = 'absolute'
   const socials = details.sociallinks
+  const [style, setStyle] = useState({
+    position: absolute,
+    left: styleData.x,
+    top: styleData.y,
+  })
+  console.log('STYLE', style)
 
   return (
     <>
-      <div>
-        <header>Event pop up card</header>
-        <li>{details.title} </li>
-        <li> {details.location}</li>
-        <li> {details.type}</li>
-        <li>{socials.instagram}</li>
-        <li>{socials.facebook}</li>
-        <li>{socials.twitter}</li>
+      <div style={style}>
+        <div className="popup">
+          <header>Event pop up card</header>
+          <li>{details.title} </li>
+          <li> {details.location}</li>
+          <li> {details.type}</li>
+          <li>{socials.instagram}</li>
+          <li>{socials.facebook}</li>
+          <li>{socials.twitter}</li>
+        </div>
       </div>
     </>
   )
