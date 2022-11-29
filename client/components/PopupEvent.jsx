922e726cd7b6b035e878b0221fd7175eccbe0c0d
Jeremy Hunter <jeremy.w.hunter@gmail.com>
2022-11-30 11:51:45 +1300

    better pop up css

--- client/components/PopupEvent.jsx
+++ client/components/PopupEvent.jsx
@@ -4,11 +4,12 @@ export default function Popup({ details, styleData, click }) {
   const absolute = 'absolute'
   const socials = details.sociallinks
   const [style, setStyle] = useState({
+    margin: 0,
+    padding: 0,
     position: absolute,
     left: styleData.x > 650 ? styleData.x - 400 : styleData.x,
     top: styleData.y > 470 ? styleData.y - 300 : styleData.y,
   })
-
 
   return (
     <>
@@ -23,8 +24,15 @@ export default function Popup({ details, styleData, click }) {
               </li>
               <li>Link: </li>
               <img src="cover.png" alt="book cover" width="30%"></img>
+              <li>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Minima deleniti possimus doloremque fuga reprehenderit dolorum
+                  obcaecati, ea est voluptas quam enim esse illum non optio
+                  doloribus illo voluptatem, earum quod!
+                </p>
+              </li>
             </ul>
-       
 
             <div className="sociallistcont">
               <ul className="sociallist">
@@ -34,7 +42,7 @@ export default function Popup({ details, styleData, click }) {
               </ul>
             </div>
           </div>
-          </div>
+        </div>
       </div>
     </>
   )
