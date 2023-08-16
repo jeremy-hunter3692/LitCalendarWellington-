4d7bb365324e22557e9a41c13b810ff797c53de3
Jeremy Hunter <jeremy.w.hunter@gmail.com>
2023-08-16 22:44:29 +1200

    bug with long event bar

--- client/components/PopupEvent.jsx
+++ client/components/PopupEvent.jsx
@@ -11,6 +11,11 @@ export default function Popup({ details, styleData, click }) {
     top: styleData.y > 470 ? styleData.y - 300 : styleData.y,
   })
 
+  console.log('length', details.about.length)
+  const about =
+    details.about.length > 400 ? details.about.slice(0, 400) : details.about
+  console.log('about length', about.length)
+
   return (
     <>
       <div style={style}>
@@ -28,7 +33,7 @@ export default function Popup({ details, styleData, click }) {
               <li>Link: {details.link} </li>
               <img src="cover.png" alt="book cover" width="30%"></img>
               <li>
-                <p>{details.about}</p>
+                <p>{about}...</p>
               </li>
             </ul>
 
